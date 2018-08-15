507a94474233875f32c1bead8b07d82b9d0fcea8
3E-6-2-113  吴佳云(10023424) <wujy@glodon.com>
2018-08-15 10:19:34 +0800

    test github acount

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,7 +1,6 @@
 package com.example.demo;
 
 import org.junit.Ignore;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -10,9 +9,5 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 @Ignore
 public class DemoApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
 
 }
